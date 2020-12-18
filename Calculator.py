bf050332c632444412b922e9b55fbd3caa494ab4
Ansh Rathi <69413769+dudeansh2@users.noreply.github.com>
2020-12-18 16:08:53 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -37,18 +37,5 @@ elif h.lower() =="substract" or h =="sub" or h=="-":
     print(sub(dif,he))
 else:
     print("invalid")
-print("made with love by a = int(input())
-b = int(input())
-c = int(input())
-def blackjack(a,b,c):
-    zz = a+b+c
-    if zz <= 21:
-        return  zz
-    elif zz > 21 and (a == 11 or b == 11 or c ==11):
-        ss = zz - 10
-        return ss
-    elif ss > 21:
-        return "BUST"
-print(blackjack(a,b,c))
 print('''Made with love by Ⓐ Ⓝ Ⓢ Ⓗ Ⓡ Ⓐ Ⓣ Ⓗ Ⓘ  
             ｇｉｔｈｕｂ ： ｄｕｄｅａｎｓｈ２''')     
